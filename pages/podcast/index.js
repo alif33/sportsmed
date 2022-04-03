9311f44adbea9678e6c705fe9569972e74680a02
siddike <absiddikhp@gmail.com>
2022-04-03 12:14:23 +0600

    podcast play page add

--- pages/podcast/index.js
+++ pages/podcast/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Layout from "./../src/components/Layout";
-
+import Layout from "./../../src/components/Layout";
+import Link from "next/link";
 const Podcast = () => {
   return (
     <>
@@ -174,104 +174,114 @@ const Podcast = () => {
               </div>
             </div>
             <div className="py-2">
-              <div className="mt-2">
-                <div className="podcastBox d-flex justify-content-center align-items-center gap-md-4 gap-2">
-                  <div className="podcastImage">
-                    <i class="fa-solid fa-play"></i>
-                    <img
-                      src="/images/UltimateLogo.png"
-                      class="img-fluid|thumbnail rounded-top|rounded-end|rounded-bottom|rounded-start|rounded-circle| "
-                      alt="not fiend"
-                    />
-                  </div>
-                  <div className="col-md-8 col-11 lh-md">
-                    <a href="#" className="text-decoration-none">
-                      <h3 className="text-white">NFL Injury Report Week 2</h3>
-                    </a>
-                    <p className="my-2">
-                      9/23/2020 | 3 min{" "}
-                      <span className="border text-white border-white px-1 py-1">
-                        latest Episode
-                      </span>
-                    </p>
-                    <p className="text-white">
-                      Dr. Chona breaks down the latest injury news from Week 2
-                      of the National Football League.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="mt-5">
-                <div className="podcastBox d-flex justify-content-center align-items-center gap-md-4 gap-2">
-                  <div className="podcastImage">
-                    <i class="fa-solid fa-play"></i>
-                    <img
-                      src="/images/UltimateLogo.png"
-                      class="img-fluid|thumbnail rounded-top|rounded-end|rounded-bottom|rounded-start|rounded-circle| "
-                      alt="not fiend"
-                    />
-                  </div>
-                  <div className="col-md-8 col-11 lh-md">
-                    <a href="#" className="text-decoration-none">
-                      <h3 className="text-white">Week 1 NFL Injury Report</h3>
-                    </a>
-                    <p>9/16/2020 | 4 min</p>
-                    <p className="text-white">
-                      Dr. Chona breaks down the most important injury news for
-                      Week 1 around the NFL.import Layout from
-                      './../src/components/Layout';
-                    </p>
+              <Link href={`podcast/${1}`}>
+                <div className="mt-2">
+                  <div className="podcastBox d-flex justify-content-center align-items-center gap-md-4 gap-2">
+                    <div className="podcastImage">
+                      <i class="fa-solid fa-play"></i>
+                      <img
+                        src="/images/UltimateLogo.png"
+                        class="img-fluid|thumbnail rounded-top|rounded-end|rounded-bottom|rounded-start|rounded-circle| "
+                        alt="not fiend"
+                      />
+                    </div>
+                    <div className="col-md-8 col-11 lh-md">
+                      <a href="#" className="text-decoration-none">
+                        <h3 className="text-white">NFL Injury Report Week 2</h3>
+                      </a>
+                      <p className="my-2">
+                        9/23/2020 | 3 min{" "}
+                        <span className="border text-white border-white px-1 py-1">
+                          latest Episode
+                        </span>
+                      </p>
+                      <p className="text-white">
+                        Dr. Chona breaks down the latest injury news from Week 2
+                        of the National Football League.
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="mt-5">
-                <div className="podcastBox d-flex justify-content-center align-items-center gap-md-4 gap-2">
-                  <div className="podcastImage">
-                    <i class="fa-solid fa-play"></i>
-                    <img
-                      src="/images/UltimateLogo.png"
-                      class="img-fluid|thumbnail rounded-top|rounded-end|rounded-bottom|rounded-start|rounded-circle| "
-                      alt="not fiend"
-                    />
-                  </div>
-                  <div className="col-md-8 col-11 lh-md">
-                    <a href="#" className="text-decoration-none">
-                      <h3 className="text-white">
-                        Kevin Durant - Performance Expectations2
-                      </h3>
-                    </a>
-                    <p>4/17/2020 | 7 min</p>
-                    <p className="text-white">
-                      Detailed post-injury performance expectations for Kevin
-                      Durant.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="mt-5">
-                <div className="podcastBox d-flex justify-content-center align-items-center gap-md-4 gap-2">
-                  <div className="podcastImage">
-                    <i class="fa-solid fa-play"></i>
-                    <img
-                      src="/images/UltimateLogo.png"
-                      class="img-fluid|thumbnail rounded-top|rounded-end|rounded-bottom|rounded-start|rounded-circle| "
-                      alt="not fiend"
-                    />
-                  </div>
-                  <div className="col-md-8 col-11 lh-md">
-                    <a href="#" className="text-decoration-none">
-                      <h3 className="text-white">
-                        Tua Tagovailoa - Injury-by-Injury Breakdown
-                      </h3>
-                    </a>
-                    <p>4/12/2020 | 9 min</p>
-                    <p className="text-white">
-                      Injury-by-injury breakdown and expectations for Tua
-                      Tagovailoa.
-                    </p>
+              </Link>
+
+              <Link href={`podcast/${2}`}>
+                <div className="mt-5">
+                  <div className="podcastBox d-flex justify-content-center align-items-center gap-md-4 gap-2">
+                    <div className="podcastImage">
+                      <i class="fa-solid fa-play"></i>
+                      <img
+                        src="/images/UltimateLogo.png"
+                        class="img-fluid|thumbnail rounded-top|rounded-end|rounded-bottom|rounded-start|rounded-circle| "
+                        alt="not fiend"
+                      />
+                    </div>
+                    <div className="col-md-8 col-11 lh-md">
+                      <a href="#" className="text-decoration-none">
+                        <h3 className="text-white">Week 1 NFL Injury Report</h3>
+                      </a>
+                      <p>9/16/2020 | 4 min</p>
+                      <p className="text-white">
+                        Dr. Chona breaks down the most important injury news for
+                        Week 1 around the NFL.import Layout from
+                        './../src/components/Layout';
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
+              </Link>
+              <Link href={`podcast/${3}`}>
+                <div className="mt-5">
+                  <div className="podcastBox d-flex justify-content-center align-items-center gap-md-4 gap-2">
+                    <div className="podcastImage">
+                      <i class="fa-solid fa-play"></i>
+                      <img
+                        src="/images/UltimateLogo.png"
+                        class="img-fluid|thumbnail rounded-top|rounded-end|rounded-bottom|rounded-start|rounded-circle| "
+                        alt="not fiend"
+                      />
+                    </div>
+                    <div className="col-md-8 col-11 lh-md">
+                      <a href="#" className="text-decoration-none">
+                        <h3 className="text-white">
+                          Kevin Durant - Performance Expectations2
+                        </h3>
+                      </a>
+                      <p>4/17/2020 | 7 min</p>
+                      <p className="text-white">
+                        Detailed post-injury performance expectations for Kevin
+                        Durant.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+
+              <Link href={`podcast/${4}`}>
+                <div className="mt-5">
+                  <div className="podcastBox d-flex justify-content-center align-items-center gap-md-4 gap-2">
+                    <div className="podcastImage">
+                      <i class="fa-solid fa-play"></i>
+                      <img
+                        src="/images/UltimateLogo.png"
+                        class="img-fluid|thumbnail rounded-top|rounded-end|rounded-bottom|rounded-start|rounded-circle| "
+                        alt="not fiend"
+                      />
+                    </div>
+                    <div className="col-md-8 col-11 lh-md">
+                      <a href="#" className="text-decoration-none">
+                        <h3 className="text-white">
+                          Tua Tagovailoa - Injury-by-Injury Breakdown
+                        </h3>
+                      </a>
+                      <p>4/12/2020 | 9 min</p>
+                      <p className="text-white">
+                        Injury-by-injury breakdown and expectations for Tua
+                        Tagovailoa.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Link>
             </div>
           </div>
         </div>
